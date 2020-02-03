47390bbc6607203efc2d992694cd1ea0b3f2f79e
mobilloper8 <dangvannghia@MacONes-MacBook-Pro.local>
2020-02-03 22:11:21 +0700

    doing

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, } from 'react-native';
+import { Button, Text, Input } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <>
-            <Text>SignUpScreen</Text>
-            <Button title="go to signin" onPress={() => navigation.navigate('SignIn')}></Button>
-        </>
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up</Text>
+            </Spacer>
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false} />
+            <Spacer />
+            <Input
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                secureTextEntry
+                autoCorrect={false} />
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
+        </View>
     );
 }
 
-const styles = StyleSheet.create({
+SignUpScreen.navigationOptions = {
+    headerShown: false
+};
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 220
+    }
 });
 
 export default SignUpScreen;
